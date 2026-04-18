15c126e4534daf28151fddff9889f36257627755
Abdallah Beraida <beriomind@gmail.com>
2026-04-18 02:12:23 +0100

    Add letters-counter tool

---
+++ print-calendar.py
@@ -0,0 +1,9 @@
+# --------------------------------------
+# - Print Calendar (print-calendar.py)
+# - Version 1
+# --------------------------------------
+
+import calendar
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+print(calendar.month(year, month))
